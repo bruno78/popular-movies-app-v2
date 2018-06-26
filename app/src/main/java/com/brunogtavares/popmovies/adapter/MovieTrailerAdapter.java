36fb79f998039f32ab358ce10f90d70c9b8ffdee
bruno78 <devbruno78@gmail.com>
2018-06-26 02:19:09 -0400

    create movie adapter

--- app/src/main/java/com/brunogtavares/popmovies/adapter/MovieTrailerAdapter.java
+++ app/src/main/java/com/brunogtavares/popmovies/adapter/MovieTrailerAdapter.java
@@ -1,13 +1,20 @@
 package com.brunogtavares.popmovies.adapter;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.brunogtavares.popmovies.R;
 import com.brunogtavares.popmovies.model.MovieTrailer;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by brunogtavares on 6/26/18.
@@ -15,38 +22,62 @@ import java.util.List;
 
 public class MovieTrailerAdapter extends RecyclerView.Adapter<MovieTrailerAdapter.ViewHolder> {
 
+    private final String LOG_TAG = MovieTrailerAdapter.class.getSimpleName();
+
     private List<MovieTrailer> mMovieTrailers;
-    private Context mContext;
+
     private final MovieTrailerOnClickHandler mClickHandler;
 
-    public interface MovieTrailerONClickHandler {
-        void o
+    public MovieTrailerAdapter(MovieTrailerOnClickHandler clickHandler) {
+        this.mClickHandler = clickHandler;
+        this.mMovieTrailers = new ArrayList<>();
+    }
+
+    public interface MovieTrailerOnClickHandler {
+        void onClick(MovieTrailer movieTrailer);
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_trailer_item, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        holder.mMovieTrailerLabel.setText("Trailer " + Integer.toString(position + 1));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mMovieTrailers == null ? 0 : mMovieTrailers.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+        @BindView(R.id.rl_trailer_button)
+        RelativeLayout movieTrailerButton;
+        @BindView(R.id.tv_movie_trailer_label)
+        TextView mMovieTrailerLabel;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-
+            int adapterPosition = getAdapterPosition();
+            MovieTrailer movieTrailer = mMovieTrailers.get(adapterPosition);
+            mClickHandler.onClick(movieTrailer);
         }
+    }
+
+    public void setMovieTrailers(List<MovieTrailer> movieTrailers) {
+        this.mMovieTrailers = movieTrailers;
+        notifyDataSetChanged();
     }
 }
